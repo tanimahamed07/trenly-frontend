b95f59edb65ff4e9636ae3b73e0358d88510230e
tanimahmed07 <tanimislamt@gmail.com>
2026-03-19 13:22:35 +0600

    all leyout file folder setup done

--- src/app/(dashboard)/dashboard/page.tsx
+++ src/app/(dashboard)/dashboard/page.tsx
@@ -1,11 +1,11 @@
 import React from 'react';
 
-const Login = () => {
+const Dashboard = () => {
     return (
         <div>
-            
+            Dashboard Layout
         </div>
     );
 };
 
-export default Login;
+export default Dashboard;
---
+++ src/app/(dashboard)/layout.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Dashboard = ({ children }: { children: React.ReactNode }) => {
+  return <div>{children}</div>;
+};
+
+export default Dashboard;
